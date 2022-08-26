cc63be94994599f7afcfd52cbbe657a70ac462db
JPhillip018 <jphillip018@gmail.com>
2022-08-26 19:51:20 -0400

    index.ejs rendered at mycloset route

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 //Dependencies
 const express = require('express')
+const shoes = require('./models/shoes')
 const app = express()
 const PORT = 3000
 
+//Default
+app.get('/', (req, res) => {
+    res.send('App is up and running!')
+})
+
+//Index - GET
+app.get('/mycloset', (req, res) => {
+    res.render('index.ejs', {
+        allShoes: shoes
+    })
+})
 
 //Listener
 app.listen(PORT, () => {

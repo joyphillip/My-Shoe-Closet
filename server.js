26bbe6e1ca64ab020670cc27f2f6349ce55657dd
JPhillip018 <jphillip018@gmail.com>
2022-08-26 19:15:25 -0400

    set up file structure and dependencies

---
+++ server.js
@@ -0,0 +1,10 @@
+//Dependencies
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+
+//Listener
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT} 🎧 `)
+})

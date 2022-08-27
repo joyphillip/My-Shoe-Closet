7406276788ade1044d22b83d1ebfff6edd9b49a0
JPhillip018 <jphillip018@gmail.com>
2022-08-27 16:34:48 -0400

    set up dotenv

--- controllers/shoesController.js
+++ controllers/shoesController.js
@@ -106,5 +106,4 @@ router.put('/:id', (req, res) => {
 })
 
 
-
-module.exports = router;
+module.exports = router
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 //Dependencies
 const express = require('express')
 const app = express()
-const PORT = 3000
 const methodOverride = require('method-override')
 const shoesController = require('./controllers/shoesController')
 
+//.env
+require('dotenv').config()
+const PORT = process.env.PORT
+
 //Mongoose
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1:27017/basiccrud', { useNewUrlParser: true});
+const mongodbURI = process.env.MONGODB_URI
+mongoose.connect(mongodbURI);
 mongoose.connection.once('open', ()=> {
     console.log('connected to mongo');
 });
@@ -20,6 +23,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
+
+//Router 
 app.use('/mycloset', shoesController)
 
 

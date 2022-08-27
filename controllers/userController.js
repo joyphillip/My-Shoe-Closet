c5521a1a5beb5df89b6795d91477edce6f385ca4
JPhillip018 <jphillip018@gmail.com>
2022-08-27 18:18:30 -0400

    user register page works

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,35 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
+const User = require('../models/users')
 const router = express.Router()
+
 
 //Default
 router.get('/register', (req, res) => {
     res.render('users/register.ejs')
 })
+
+//Create - POST
+router.post('/register', (req, res) => {
+    const salt = bcrypt.genSaltSync(10)
+    req.body.password = bcrypt.hashSync(req.body.password, salt)
+    // console.log(req.body)
+    User.findOne({username: req.body.username}, (error, userExists) => {
+       if(userExists) {
+        res.send('Username is taken. Please try again')
+       } else {
+        User.create(req.body, (error, createdUser) => {
+            // console.log(createdUser)
+            // res.send('User created')
+            res.redirect('/mycloset')
+        })
+       }
+    })
+})
+
+
+
+
+
 
 module.exports = router
